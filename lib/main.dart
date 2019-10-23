badf8b14624c60e15d3208f88dc697cd803c7398
n-olivier <onshimiyimana17@alustudent.com>
2019-10-23 05:26:16 +0200

    made home HomePage class stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,110 @@
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(Calculator());
 }
 
-class Calculator extends StatelessWidget{
-
+class Calculator extends StatelessWidget {
   @override
-  build(context){ 
-      return MaterialApp(
-        home: HomePage()
-      );
+  build(context) {
+    return MaterialApp(home: HomePage());
   }
 }
 
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Home();
+  }
+}
 
-class HomePage extends StatelessWidget{
-    build(context){
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Calculator"),
-            centerTitle: true,
-          ),
-
-          body: Center(
-            child: Column(
+class _Home extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Calculator"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
             children: [
-             Container(
-               width: double.infinity,
-               height: 80,
-               decoration: BoxDecoration(
-                 border: Border.all(width: 2)
-               ),
-               child:  Center(
-                 child: Text("Value goes here", style: TextStyle(fontSize: 30, color: Colors.red),),
-               )
-             ),
-              Wrap(
-                children: makeButtons()
-              ),
+              Container(
+                  width: double.infinity,
+                  height: 80,
+                  decoration: BoxDecoration(border: Border.all(width: 2)),
+                  child: Center(
+                    child: Text(
+                      "Value goes here",
+                      style: TextStyle(fontSize: 30, color: Colors.red),
+                    ),
+                  )),
+              Wrap(children: makeButtons()),
             ],
           ),
-          )
-        );
-    }
+        ));
+  }
 
-    makeButtons(){
-      List<Widget> btns = [];
-      List<Widget> btns2 = [
+  makeButtons() {
+    List<Widget> btns = [];
+    List<Widget> btns2 = [
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("+"),
+          onPressed: () {},
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("0"),
+          onPressed: () {},
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("="),
+          onPressed: () {},
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("*"),
+          onPressed: () {},
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("/"),
+          onPressed: () {},
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.all(10),
+        child: RaisedButton(
+          child: Text("CLEAR"),
+          onPressed: () {},
+        ),
+      ),
+    ];
+    for (int i = 1; i <= 9; i++) {
+      btns.add(
         Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("+"),
-                    onPressed: (){},
-                  ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("0"),
-                    onPressed: (){},
-                  ),
-                  )
-                  ,Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("="),
-                    onPressed: (){},
-                  ),
-                  )
-                  ,Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("*"),
-                    onPressed: (){},
-                  ),
-                  )
-                  ,Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("/"),
-                    onPressed: (){},
-                  ),
-                  )
-                  ,Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("CLEAR"),
-                    onPressed: (){},
-                  ),
-                  ),
-      ];
-      for(int i=1; i <= 9; i++){
-          btns.add(
-            Padding(
-                    padding: EdgeInsets.all(10),
-                    child: RaisedButton(
-                    child: Text("$i"),
-                    onPressed: (){},
-                  ),
-                  ),
-          );
-      }
-
-      return btns + btns2;
+          padding: EdgeInsets.all(10),
+          child: RaisedButton(
+            child: Text("$i"),
+            onPressed: () {},
+          ),
+        ),
+      );
     }
+
+    return btns + btns2;
+  }
 }
